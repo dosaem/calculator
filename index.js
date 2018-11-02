761a2dddfe35d4d29c2044bebc8aab2598399dfc
dosaem <dosaem90@gmail.com>
2018-11-02 22:04:48 +0900

    한자리수 더하기 완성

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ window.onload = function() {
 
 
 var str2 = "";
+var num = 0;
+var sum = 0;
 function click(e){
   var str = this.innerHTML;
   str2 = str2.concat(str);
   document.getElementById("ivalue").innerHTML = str2;
+
+  if(str.charCodeAt() > 47 && str.charCodeAt() < 58) {
+    num = Number(str);
+  }
+  else if(str.charCodeAt() == 43) {
+    sum = sum + num;
+    console.log(sum);
+  }
+  else if(str.charCodeAt() == 61) {
+    sum = sum + num;
+    document.getElementById("ivalue").innerHTML = sum;
+  }
 }
 }

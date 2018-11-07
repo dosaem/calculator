8be5db3b58cf29071d62cf163fa580d98730c80e
dosaem <dosaem90@gmail.com>
2018-11-07 16:28:03 +0900

    이전코멘트에 맞게 변수이름 수정, dom 탐색 최적화 재시도

--- index.js
+++ index.js
@@ -13,29 +13,23 @@ var num = 0;
 var sum = 0;
 var multi = 1;
 var result = 0;
+var iV = getIvalue();
 
 function click(e){
   var str = this.innerHTML;
-  var IV = getIvalue(e);
   str2 += this.innerHTML;
-  IV.innerHTML = str2;
-  
-  
-  console.log(typeof(str));
+  iV.innerHTML = str2;
+
   // 숫자
   if(str.charCodeAt() > 47 && str.charCodeAt() < 58) {
     stnum += str;
     num = Number(stnum);
-    console.log(stnum);
-    console.log(str);
-    console.log(num);
   }
   // 더하기
   else if(str.charCodeAt() == 43) {
     preBt = str;
     sum = sum + num;
     stnum = 0;
-    console.log(sum);
   }
 
   // 곱하기
@@ -43,7 +37,6 @@ function click(e){
     preBt = str;
     multi = multi * num;
     stnum = 0;
-    console.log(multi);
   }
 
   // 등호
@@ -54,23 +47,24 @@ function click(e){
     else if(preBt.charCodeAt() == 42) {
       result = multi * num;
     }
-    IV.innerHTML = result;
+    iV.innerHTML = result;
   }
 
   // AC, 올클리어
   else if(str == "AC") {
-    IV.innerHTML = 0;
+    iV.innerHTML = 0;
     str = 0;
     str2 = "";
     num = 0;
     sum = 0;
     stnum = 0;
+    multi = 1;
+    result = 0;
   }
 }
 
 function getIvalue(e) {
   return document.getElementById("ivalue");
 }
-
 }
 )

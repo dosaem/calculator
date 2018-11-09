38f801ba097c3773784a76cda80676d698f60ddc
dosaem <dosaem90@gmail.com>
2018-11-09 22:16:50 +0900

    더하기 가능

--- index.js
+++ index.js
@@ -7,17 +7,39 @@ window.addEventListener('DOMContentLoaded', function() {
 
 var iv = document.getElementById("ivalue");
 var stringNum = "";
+var preButton ="";
+var preButtonCount = 0;
+var sum = 0;
 
 function click(e){
   // 입력 받은 버튼 값
   var inputValue = this.innerHTML;
+  console.log(inputValue);
+
 
   // 숫자를 구분하고 화면에 출력.
-  if(inputValue.charCodeAt() > 47 && inputValue.charCodeAt() < 58) {
+  if(inputValue >= "0" && inputValue <= "9") {
     stringNum += inputValue;
     num = Number(stringNum);
     iv.innerHTML = num;
+    if(preButton == "+") {
+      sum += num;
+    }
   }
+
+  else if(inputValue == "+") {
+    preButtonCount++;
+    preButton = inputValue;
+    stringNum = "";
+    if(preButtonCount == 1) {
+      sum += num;
+    }
+    if(preButtonCount > 1) {
+      iv.innerHTML = sum;
+    }
+  }
+
+
 }
 }
 )

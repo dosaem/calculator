a3357e3cf248d17db25525114b65eb157d7ef752
shine1594 <panamera1594@gmail.com>
2018-11-10 00:05:13 +0900

    계산기 1차 완성

--- index.js
+++ index.js
@@ -1,23 +1,154 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var cols = document.querySelectorAll('div.calculator-button > button'); 
- 
-  Array.prototype.forEach.call(cols ,function(col){
-    col.addEventListener("click",click,false);
-});
+window.addEventListener("DOMContentLoaded", function() {
+  var calculator = {
+    numbers: [0],
+    operators: [],
+    _getLastNumber: function() {
+      return this.numbers[this.numbers.length - 1];
+    },
+    _getLastOperator: function() {
+      return this.operators[this.operators.length - 1];
+    },
+    _concatNumber: function(num, str) {
+      return parseInt(String(num) + str, 10);
+    },
+    _calculate: function(operator, right, left) {
+      switch (operator) {
+        case "add":
+          return left + right;
+        case "substract":
+          return left - right;
+        case "multiply":
+          return left * right;
+        case "divide":
+          return left / right;
+      }
+    },
+    _handleLowerPriorityOperator: function() {
+      var numberCount = this.numbers.length;
+      var operatorCount = this.operators.length;
 
-var iv = document.getElementById("ivalue");
-var stringNum = "";
+      if (numberCount === operatorCount) {
+        this.operators.pop();
+      } else if (operatorCount && numberCount > operatorCount) {
+        this.numbers.push(
+          this._calculate(
+            this.operators.pop(),
+            this.numbers.pop(),
+            this.numbers.pop()
+          )
+        );
+      } else {
+        console.log(this.numbers, this.operators);
+      }
 
-function click(e){
-  // 입력 받은 버튼 값
-  var inputValue = this.innerHTML;
+      return this;
+    },
+    _isHigherPriorityOperator: function() {
+      var op = this._getLastOperator();
+      return op === "multiply" || op === "divide";
+    },
+    _handleHigherPriorityOperator: function() {
+      var numberCount = this.numbers.length;
+      var operatorCount = this.operators.length;
 
-  // 숫자를 구분하고 화면에 출력.
-  if(inputValue.charCodeAt() > 47 && inputValue.charCodeAt() < 58) {
-    stringNum += inputValue;
-    num = Number(stringNum);
-    iv.innerHTML = num;
+      if (numberCount === operatorCount) {
+        this.operators.pop();
+      } else if (
+        operatorCount &&
+        numberCount > operatorCount &&
+        this._isHigherPriorityOperator()
+      ) {
+        this.numbers.push(
+          this._calculate(
+            this.operators.pop(),
+            this.numbers.pop(),
+            this.numbers.pop()
+          )
+        );
+      } else {
+        console.log(this.numbers, this.operators);
+      }
+
+      return this;
+    },
+    inputValue: function(input) {
+      if (this.numbers.length === this.operators.length) {
+        this.numbers.push(parseInt(input, 10));
+      } else {
+        this.numbers.push(this._concatNumber(this.numbers.pop(), input));
+      }
+
+      return this;
+    },
+    clear: function() {
+      this.numbers = [0];
+      this.operators = [];
+      return this;
+    },
+    toggleSign: function() {
+      this.numbers.push(this.numbers.pop() * -1);
+      return this;
+    },
+    percent: function() {
+      this.numbers.push(this.numbers.pop() / 100);
+      return this;
+    },
+    add: function() {
+      this._handleLowerPriorityOperator();
+      this.operators.push("add");
+      return this;
+    },
+    substract: function() {
+      this._handleLowerPriorityOperator();
+      this.operators.push("substract");
+      return this;
+    },
+    multiply: function() {
+      this._handleHigherPriorityOperator();
+      this.operators.push("multiply");
+      return this;
+    },
+    divide: function() {
+      this._handleHigherPriorityOperator();
+      this.operators.push("divide");
+      return this;
+    },
+    enter: function _enter() {
+      if (this.operators.length === 0) return this;
+
+      if (this.operators.length === this.numbers.length) {
+        this.operators.pop();
+      } else {
+        this.numbers.push(
+          this._calculate(
+            this.operators.pop(),
+            this.numbers.pop(),
+            this.numbers.pop()
+          )
+        );
+      }
+
+      _enter.call(this);
+    }
+  };
+
+  var $view = document.getElementById("ivalue");
+
+  function click() {
+    var operator = this.dataset.operator;
+    if (operator) {
+      calculator[operator].call(calculator);
+    } else {
+      calculator.inputValue(this.innerHTML);
+    }
+
+    $view.innerHTML = calculator._getLastNumber();
   }
-}
-}
-)
+
+  Array.prototype.forEach.call(
+    document.querySelectorAll("div.calculator-button > button"),
+    function(col) {
+      col.addEventListener("click", click, false);
+    }
+  );
+});

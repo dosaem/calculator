51c42a203fcc78d5780ccec6b0f8a555ebbdc8ac
dosaem <dosaem90@gmail.com>
2018-11-09 17:36:06 +0900

    숫자버튼 구분 및 화면에 출력 처리

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 var iv = document.getElementById("ivalue");
+var stringNum = "";
 
 function click(e){
-  // 화면표시부
-  var str = this.innerHTML;
-  iv.innerHTML = str;
+  // 입력 받은 버튼 값
+  var inputValue = this.innerHTML;
+
+  // 숫자를 구분하고 화면에 출력.
+  if(inputValue.charCodeAt() > 47 && inputValue.charCodeAt() < 58) {
+    stringNum += inputValue;
+    num = Number(stringNum);
+    iv.innerHTML = num;
+  }
 }
 }
 )

d1723db930acfb90758c79b27e4110735c4acebc
dosaem <dosaem90@gmail.com>
2018-11-10 11:19:42 +0900

    곱하기 가능

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ window.addEventListener('DOMContentLoaded', function() {
 var iv = document.getElementById("ivalue");
 var stringNum = "";
 var preButton ="";
+var preValue = 0;
 var plusButtonCount = 0;
 var multiButtonCount = 0;
-var sum = 0;
+var sum = null;
+var multi = 1;
+var num = null;
 
 function click(e){
   // 입력 받은 버튼 값
   var inputValue = this.innerHTML;
-  console.log(inputValue);
-
 
   // 숫자를 구분하고 화면에 출력.
   if(inputValue >= "0" && inputValue <= "9") {
@@ -25,6 +26,9 @@ function click(e){
     iv.innerHTML = num;
     if(preButton == "+") {
       sum += num;
+    }
+    else if(preButton == "*") {
+      multi *= num;
     }
   }
 
@@ -42,11 +46,17 @@ function click(e){
   }
 
   // 곱하기
-  // else if(inputValue == "*") {
-  //   multiButtonCount++;
-
-
-  // }
+  else if(inputValue == "*") {
+    multiButtonCount++;
+    preButton = inputValue;
+    stringNum = "";
+    if(multiButtonCount == 1) {
+      multi *= num;
+      }
+    else if(multiButtonCount > 1) {
+      iv.innerHTML = multi;
+    }
+  }
 
   // = 버튼
   else if(inputValue == "=") {
@@ -54,7 +64,13 @@ function click(e){
       iv.innerHTML = sum;
       preButton = "";
       plusButtonCount = 0;
-      num = 0;
+      num = null;
+    }
+    else if(preButton == "*") {
+      iv.innerHTML = multi;
+      preButton = "";
+      multiButtonCount = 0;
+      num = null;
     }
 
   }
@@ -63,11 +79,11 @@ function click(e){
     stringNum = "";
     preButton ="";
     plusButtonCount = 0;
-    sum = 0;
+    multiButtonCount = 0;
+    sum = null;
+    multi = 1;
     iv.innerHTML = 0;
   }
-
-
 }
 }
 )

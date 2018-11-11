2cd63fbfdded235c52b5c72d0b5d6641b43866f3
dosaem <dosaem90@gmail.com>
2018-11-11 22:59:17 +0900

    전면수정

--- index.js
+++ index.js
@@ -7,82 +7,40 @@ window.addEventListener('DOMContentLoaded', function() {
 
 var iv = document.getElementById("ivalue");
 var stringNum = "";
-var preButton ="";
-var preValue = 0;
-var plusButtonCount = 0;
-var multiButtonCount = 0;
-var sum = null;
-var multi = 1;
-var num = null;
+var num0 = null;
+var num1 = null;
+var num2 = null;
+var opercount = 0;
 
 function click(e){
   // 입력 받은 버튼 값
   var inputValue = this.innerHTML;
 
-  // 숫자를 구분하고 화면에 출력.
-  if(inputValue >= "0" && inputValue <= "9") {
-    stringNum += inputValue;
-    num = Number(stringNum);
-    iv.innerHTML = num;
-    if(preButton == "+") {
-      sum += num;
-    }
-    else if(preButton == "*") {
-      multi *= num;
+  // 숫자
+  if(inputValue >= "0" && inputValue <= "9" || inputValue == ".") {
+    switch(opercount) {
+      case 0 :
+      stringNum += inputValue;
+      num0 = Number(stringNum);
+      iv.innerHTML = num0;
+      break;
+      case 1 :
+      stringNum += inputValue;
+      num1 = Number(stringNum);
+      iv.innerHTML = num1;
+      break;
+      case 2 :
+      stringNum += inputValue;
+      num2 = Number(stringNum);
+      iv.innerHTML = num2;
+      break;
     }
   }
 
-  // 더하기
-  else if(inputValue == "+") {
-    plusButtonCount++;
-    preButton = inputValue;
+  // 숫자이외
+  else {
+    opercount++;
     stringNum = "";
-    if(plusButtonCount == 1) {
-      sum += num;
-    }
-    else if(plusButtonCount > 1) {
-      iv.innerHTML = sum;
-    }
-  }
-
-  // 곱하기
-  else if(inputValue == "*") {
-    multiButtonCount++;
-    preButton = inputValue;
-    stringNum = "";
-    if(multiButtonCount == 1) {
-      multi *= num;
-      }
-    else if(multiButtonCount > 1) {
-      iv.innerHTML = multi;
-    }
-  }
-
-  // = 버튼
-  else if(inputValue == "=") {
-    if(preButton == "+") {
-      iv.innerHTML = sum;
-      preButton = "";
-      plusButtonCount = 0;
-      num = null;
-    }
-    else if(preButton == "*") {
-      iv.innerHTML = multi;
-      preButton = "";
-      multiButtonCount = 0;
-      num = null;
-    }
-
-  }
-  // AC 올클리어
-  else if(inputValue == "AC") {
-    stringNum = "";
-    preButton ="";
-    plusButtonCount = 0;
-    multiButtonCount = 0;
-    sum = null;
-    multi = 1;
-    iv.innerHTML = 0;
   }
 }
 }

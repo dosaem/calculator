ccb9c824e9198e8e3a8603c191e963f3dd35c719
dosaem <dosaem90@gmail.com>
2018-11-12 12:30:44 +0900

    더하기 곱하기 가능

--- index.js
+++ index.js
@@ -7,40 +7,82 @@ window.addEventListener('DOMContentLoaded', function() {
 
 var iv = document.getElementById("ivalue");
 var stringNum = "";
-var num0 = null;
-var num1 = null;
-var num2 = null;
-var opercount = 0;
+var inputValue = "";
+var num = [];
+var numCount = 0;
+var oper = [];
+var operCount = 0;
+
+
+function inputNumber() {
+    stringNum += inputValue;
+    num[operCount] = Number(stringNum);
+    iv.innerHTML = num[operCount];
+    numCount++;
+    console.log(num[0]);
+    console.log(num[1]);
+    console.log(num[2]);
+  }
+
+function inputOper() {
+  operCount++;
+  stringNum = "";
+  oper[operCount] = inputValue;
+  console.log(oper[1]);
+  console.log(oper[2]);
+}
+
+function saemOper() {
+  switch (oper[1]) {
+    case "+" :
+      num[0] += num[1];
+      iv.innerHTML = num[0];
+      operCount--;
+      numCount--;
+      break;
+    case "*" :
+      num[0] *= num[1];
+      iv.innerHTML = num[0];
+      operCount--;
+      numCount--;
+      break;
+  }
+}
+
+function diffOper() {
+  if(oper[1] == "+") {
+    switch (oper[2]) {
+      case "*" :
+      num[1] *= num[2];
+      console.log(num[1]);
+      num[0] += num[1];
+      console.log(num[0]);
+      iv.innerHTML = num[0];
+      operCount--;
+      numCount--;
+    }
+  }   
+  
+}
 
 function click(e){
   // 입력 받은 버튼 값
-  var inputValue = this.innerHTML;
+  inputValue = this.innerHTML;
 
   // 숫자
   if(inputValue >= "0" && inputValue <= "9" || inputValue == ".") {
-    switch(opercount) {
-      case 0 :
-      stringNum += inputValue;
-      num0 = Number(stringNum);
-      iv.innerHTML = num0;
-      break;
-      case 1 :
-      stringNum += inputValue;
-      num1 = Number(stringNum);
-      iv.innerHTML = num1;
-      break;
-      case 2 :
-      stringNum += inputValue;
-      num2 = Number(stringNum);
-      iv.innerHTML = num2;
-      break;
-    }
+     inputNumber(inputValue);
+     if(operCount == 2 && oper[1] != oper[2] && numCount == 3) {
+      diffOper();
+    } 
   }
 
   // 숫자이외
   else {
-    opercount++;
-    stringNum = "";
+    inputOper(inputValue);
+    if(operCount == 2 && oper[1] == oper[2] && numCount == 2) {
+      saemOper();
+    }
   }
 }
 }

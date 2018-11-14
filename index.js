34855a6f117d2b5786ef9c68fe84bb46d624f826
dosaem <dosaem90@gmail.com>
2018-11-14 17:48:49 +0900

    사칙연산 오류 수정

--- index.js
+++ index.js
@@ -7,93 +7,83 @@ window.addEventListener('DOMContentLoaded', function() {
 
 var iv = document.getElementById("ivalue");
 var stringNum = "";
-var num = [];
-var numIndex = 0;
+var numArray = [];
 var oper = [];
 
 
 function inputNumber(inputValue) {
-  if(num.length == oper.length) {
-    stringNum += inputValue;
-    num.push(Number(stringNum));
-    var numLength = num.length;
-    iv.innerHTML = num[numLength-1];
-  }    
-  }
+  stringNum += inputValue;
+  num = Number(stringNum);
+  iv.innerHTML = num;
+}
 
 function inputOper(inputValue) {
-  // numIndex++;
   stringNum = "";
   oper.push(inputValue);
 }
 
 function init() {
-  console.log(num);
-  console.log(oper);
-  iv.innerHTML = num[0];
+  iv.innerHTML = numArray[0];
   oper.pop();
-  num.pop();
-  console.log(num);
-  console.log(oper);
+  numArray.pop();
 }
 
 
 
 function operlator() {
     if(oper[0] == "+" && oper[1] == "+"){
-      num[0] += num[1];
+      numArray[0] += numArray[1];
       init();
     }
     else if(oper[0] == "+" && oper[1] == "-") {
-      num[0] += num[1];
+      numArray[0] += numArray[1];
       oper[0] = oper[1];
       init();
     }
     else if(oper[0] == "-" && oper[1] == "+") {
-      num[0] -= num[1];
+      numArray[0] -= numArray[1];
       oper[0] = oper[1];
       init();
     }
     else if(oper[0] == "-" &&oper[1] == "-") {
-      num[0] -= num[1];
+      numArray[0] -= numArray[1];
       init();
     }
     else if(oper[0] == "*") {
-      num[0] *= num[1];
+      numArray[0] *= numArray[1];
       oper[0] = oper[1];
       init();
     }
     else if(oper[0] == "/") {
-      num[0] /= num[1];
+      numArray[0] /= numArray[1];
       oper[0] = oper[1];
       init();
     }
     if((oper[0] == "+" || oper[0] == "-") && oper[1] == "*" && oper.length == 3){
-      num[1] *= num[2];
-      iv.innerHTML = num[2];
+      numArray[1] *= numArray[2];
+      iv.innerHTML = numArray[2];
       oper[1] = oper[2];
-      console.log("Error");
       init();
       if(oper[1] == "+" || oper[1] == "-") {
         operlator();       
       }
       else if(oper[1] == "*" || oper[1] == "/") {
-        iv.innerHTML = num[1];
+        iv.innerHTML = numArray[1];
       }
       else if(oper[1] == "=") {
         equlOper();      
       }
     }
     else if((oper[0] == "+" || oper[0] == "-") && oper[1] == "/" && oper.length == 3) {
-      num[1] /= num[2];
-      iv.innerHTML = num[2];
+      numArray[1] /= numArray[2];
+      iv.innerHTML = numArray[2];
       oper[1] = oper[2];
       init();
       if(oper[1] == "+" || oper[1] == "-") {
         operlator();       
       }
       else if(oper[1] == "*" || oper[1] == "/") {
-        iv.innerHTML = num[1];
+        iv.innerHTML = numArray[1];
       }
       else if(oper[1] == "=") {
         equlOper();      
@@ -103,50 +93,35 @@ function operlator() {
 
   function initAc() {
       stringNum = "";
-      num = [];
-      // numIndex = 0;
+      numArray = [];
       oper = [];
       iv.innerHTML = 0;
-      console.log(oper);
-      console.log(num);
-
   }
 
   function equlOper() {
     switch(oper[0]) {
       case "+" :
-      var initV = num[0] + num[1];
+      num = numArray[0] + numArray[1];
       initAc();
-      num[0] = initV;
-      iv.innerHTML = num[0];
-      console.log(num);
+      iv.innerHTML = num;
       break;
       case "-" :
-      var initV = num[0] - num[1];
+      num = numArray[0] - numArray[1];
       initAc();
-      num[0] = initV;
-      iv.innerHTML = num[0];
-      console.log(num);
+      iv.innerHTML = num;
       break;
       case "*" :
-      var initV = num[0] * num[1];
+      num = numArray[0] * numArray[1];
       initAc();
-      num[0] = initV;
-      iv.innerHTML = num[0];
-      console.log(num);
+      iv.innerHTML = num;
       break;
       case "/" :
-      var initV = num[0] / num[1];
+      num = numArray[0] / numArray[1];
       initAc();
-      num[0] = initV;
-      iv.innerHTML = num[0];
-      console.log(num);
+      iv.innerHTML = num;
       break;
     }
   }
-
-
-
 
 function click(e){
   // 입력 받은 버튼 값
@@ -154,25 +129,24 @@ function click(e){
 
   // 숫자받기
   if(inputValue >= "0" && inputValue <= "9" || inputValue == ".") {
-    console.log(num);
-     inputNumber(inputValue);
-     console.log("hi");
-     console.log(num);
+    inputNumber(inputValue);
   }
 
   // 숫자이외
   else {
+    console.log(num);
+    numArray.push(num);
     inputOper(inputValue);
-    console.log(oper.length);
-    console.log(oper);
     if(inputValue == "AC") {
       initAc();
     }
-      if(oper.length == 2 && inputValue == "=") {
-        equlOper();
-      }
+    else if(oper.length == 2 && inputValue == "=") {
+      equlOper();
+    }
+    else if(oper.length == 2 && inputValue != "="){
       operlator();
     }
-    } 
+    }
+  } 
   }
 )
